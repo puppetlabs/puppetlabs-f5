799655929da8e558c960502174f2332c6a2d6dba
Gary Larizza <gary@puppetlabs.com>
2011-08-19 11:58:39 -0700

    Add f5::config defined resource type
    This commit adds the f5::config defined resource type and the
    config.erb template references in the type.

---
+++ manifests/config.pp
@@ -0,0 +1,41 @@
+# Defined Resource Type: f5::config
+#
+#   This defined resource type will create an f5 device configuration file
+#     to be used with Puppet. 
+#
+# Parameters:
+#
+# [*username*] - The username used to connect to the f5 device
+# [*password*] - The password used to connect to the f5 device
+# [*url*]      - The url to the f5 device. DO NOT INCLUDE https://
+# [*target*]   - The path to the f5 configuration file we are creating
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  f5::config { 'bigip':
+#    username => 'admin',
+#    password => 'password',
+#    url      => 'f5.puppetlabs.lan',
+#    target   => '/etc/puppetlabs/puppet/device/bigip.conf
+#  }
+#
+define f5::config(
+  $username = 'admin',
+  $password,
+  $url,
+  $target
+) {
+
+  file { $target:
+    ensure => present,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0644',
+    content => template('f5/config.erb'),
+  }
+    
+}
