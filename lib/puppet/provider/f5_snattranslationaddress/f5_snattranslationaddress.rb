9ceb3cce7bd45570a2b624d538f5d1ce0e3069c2
Nan Liu <nan@puppetlabs.com>
2011-08-30 15:34:35 -0400

    Fix F5_WSDL constant, and invalid require.
    The F5_WSDL constant worked if a single F5 resource type changed, this
    change allows multiple resource type to be modified. Also several
    provider had the inappropriate require and this address that as well.

--- lib/puppet/provider/f5_snattranslationaddress/f5_snattranslationaddress.rb
+++ lib/puppet/provider/f5_snattranslationaddress/f5_snattranslationaddress.rb
@@ -1,5 +1,4 @@
-require 'f5-icontrol'
-require 'util/network_device/f5.rb'
+require 'puppet/provider/f5'
 
 Puppet::Type.type(:f5_snattranslationaddress).provide(:f5_snattranslationaddress, :parent => Puppet::Provider::F5) do
   @doc = "Manages f5 snattranslationaddress"
@@ -7,10 +6,16 @@ Puppet::Type.type(:f5_snattranslationaddress).provide(:f5_snattranslationaddress
   confine :feature => :posix
   defaultfor :feature => :posix
 
-  F5_WSDL = 'LocalLB.SNATTranslationAddress'
+  def self.wsdl
+    'LocalLB.SNATTranslationAddress'
+  end
+
+  def wsdl
+    self.class.wsdl
+  end
 
   def self.instances
-    transport[F5_WSDL].get_list.collect do |name|
+    transport[wsdl].get_list.collect do |name|
       new(:name => name)
     end
   end
@@ -24,44 +29,44 @@ Puppet::Type.type(:f5_snattranslationaddress).provide(:f5_snattranslationaddress
 
   methods.each do |method|
     define_method(method.to_sym) do
-      if transport[F5_WSDL].respond_to?("get_#{method}".to_sym)
-        transport[F5_WSDL].send("get_#{method}", resource[:name]).first
+      if transport[wsdl].respond_to?("get_#{method}".to_sym)
+        transport[wsdl].send("get_#{method}", resource[:name]).first
       end
     end
   end
 
   methods.each do |method|
     define_method("#{method}=") do |value|
-      if transport[F5_WSDL].respond_to?("set_#{method}".to_sym)
-        transport[F5_WSDL].send("set_#{method}", resource[:name], resource[method.to_sym])
+      if transport[wsdl].respond_to?("set_#{method}".to_sym)
+        transport[wsdl].send("set_#{method}", resource[:name], resource[method.to_sym])
       end
     end
   end
 
   def connection_limit
-    val = transport[F5_WSDL].get_connection_limit(resource[:name]).first
+    val = transport[wsdl].get_connection_limit(resource[:name]).first
     [val.high, val.low]
   end
 
   def connection_limit=(value)
-    val = transport[F5_WSDL].set_connection_limit(resource[:name], resource[:connection_limit])
+    val = transport[wsdl].set_connection_limit(resource[:name], resource[:connection_limit])
   end
 
   def create
     Puppet.debug("Puppet::Provider::F5_SNATTranslationAddress: creating F5 snattranslationaddress #{resource[:name]}")
-    transport[F5_WSDL].create(resource[:name])
+    transport[wsdl].create(resource[:name])
   end
 
   def destroy
     Puppet.debug("Puppet::Provider::F5_SNATTranslationAddress: destroying F5 snattranslationaddress #{resource[:name]}")
-    transport[F5_WSDL].delete_translation_address(resource[:name])
+    transport[wsdl].delete_translation_address(resource[:name])
   end
 
   def ensure
-    transport[F5_WSDL].get_enabled_state(resource[:name])
+    transport[wsdl].get_enabled_state(resource[:name])
   end
 
   def exists?
-    transport[F5_WSDL].get_list.include?(resource[:name])
+    transport[wsdl].get_list.include?(resource[:name])
   end
 end
