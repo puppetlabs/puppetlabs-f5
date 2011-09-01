10256c7241871b59c0946fd1fb60fc6ec63f600c
Brenton Leanhardt <bleanhar@redhat.com>
2011-09-01 16:39:51 -0400

    Adding a first pass at f5_profileclientssl
    I tested it with the following code:
    
    f5_profileclientssl { "demo2":
      ensure                  => present,
      certificate_file        => {"value" => "demo2-server.crt", "default_flag" => false},
      key_file                => {"value" => "demo2-server.key", "default_flag" => false},
      ca_file                 => {"value" => "demo2-ca.crt", "default_flag" => false},
      peer_certification_mode => {"value" => "CLIENTSSL_CERTIFICATE_MODE_AUTO", "default_flag" => false},
    }
    
    I tried to stick as close to the iControl API as possible.  In doing so I have
    made it so that the values of the parameters take the ProfileString.  This
    allows for easy comparison since the data structure returned from the icontrol
    gem also works with hashes.

---
+++ lib/puppet/provider/f5_profileclientssl/f5_profileclientssl.rb
@@ -0,0 +1,86 @@
+require 'puppet/provider/f5'
+
+Puppet::Type.type(:f5_profileclientssl).provide(:f5_profileclientssl, :parent => Puppet::Provider::F5) do
+  @doc = "Manages f5 device"
+
+  confine :feature => :posix
+  defaultfor :feature => :posix
+
+  def self.wsdl
+    'LocalLB.ProfileClientSSL'
+  end
+
+  def wsdl
+    self.class.wsdl
+  end
+
+  def self.instances
+    transport[wsdl].get_list.collect do |name|
+      new(:name => name)
+    end
+  end
+
+  methods = [
+    'certificate_file',
+    'key_file',
+    'ca_file',
+    'client_certificate_ca_file',
+    'peer_certification_mode',
+  ]
+
+  methods.each do |method|
+    define_method(method.to_sym) do
+      if transport[wsdl].respond_to?("get_#{method}".to_sym)
+        profile_string = transport[wsdl].send("get_#{method}", resource[:name]).first
+        {"value" => profile_string.value, "default_flag" => profile_string.default_flag}
+      end
+    end
+  end
+
+  methods.each do |method|
+    define_method("#{method}=") do |profile_string|
+      if transport[wsdl].respond_to?("set_#{method}".to_sym)
+        transport[wsdl].send("set_#{method}",
+                              resource[:name],
+                              [:value => profile_string["value"],
+                               :default_flag => profile_string["default_flag"]])
+      end
+    end
+  end
+
+  # Aliases for API inconsistencies
+  class << transport[wsdl]
+    alias_method :set_peer_certification_mode, :set_peer_certificate_mode
+  end
+
+  def create
+    Puppet.debug("Puppet::Provider::F5_ProfileClientSSL: creating F5 client ssl profile #{resource[:name]}")
+
+    transport[wsdl].create([resource[:name]],
+                           [:value => resource[:key_file]["value"],
+                            :default_flag => resource[:key_file]["default_flag"]],
+                           [:value => resource[:certificate_file]["value"],
+                            :default_flag => resource[:certificate_file]["default_flag"]])
+
+    # It's not clear to me the difference between these two.  We've been
+    # setting them to be the same thing.
+    ca = resource[:ca_file] || resource[:client_certificate_ca_file]
+    if ca
+      self.ca_file = ca
+      self.client_certificate_ca_file = ca
+    end
+
+    if resource[:peer_certification_mode]
+      self.peer_certification_mode = resource[:peer_certification_mode]
+    end
+  end
+
+  def destroy
+    Puppet.debug("Puppet::Provider::F5_ProfileClientSSL: destroying F5 client ssl profile #{resource[:name]}")
+    transport[wsdl].delete_profile([resource[:name]])
+  end
+
+  def exists?
+    transport[wsdl].get_list.include?(resource[:name])
+  end
+end
