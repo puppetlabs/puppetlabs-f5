37d5b3f5c301a031bca647617281f07f522b9282
Brenton Leanhardt <bleanhar@redhat.com>
2011-09-01 16:39:51 -0400

    Adding a first pass at f5_profileclientssl
    I tested it with the following code:
    
    f5_profileclientssl { "demo2":
      ensure     => present,
      certificate_file => "demo2-server.crt:false",
      key_file   => "demo2-server.key:false",
      ca_file    => "demo2-ca.crt:false",
      peer_certification_mode => "CLIENTSSL_CERTIFICATE_MODE_AUTO:false",
    }

---
+++ lib/puppet/type/f5_profileclientssl.rb
@@ -0,0 +1,44 @@
+Puppet::Type.newtype(:f5_profileclientssl) do
+  @doc = "Manage F5 Client SSL profiles."
+
+  apply_to_device
+
+  ensurable do
+    desc "Add or delete Client SSL profile."
+
+    defaultto(:present)
+
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+  end
+
+  newparam(:name, :namevar=>true) do
+    desc "The Client SSL profile name."
+  end
+
+  newproperty(:certificate_file) do
+    desc "Sets the certificate filenames to be used by BIG-IP acting as an SSL server."
+  end
+
+  newproperty(:key_file) do
+    desc "Sets the key filenames to be used by BIG-IP acting as an SSL server. If a full path is not specified, the file name is relative to /config/ssl/ssl.key."
+  end
+
+  newproperty(:ca_file) do
+    desc "The CA to use to validate client certificates"
+  end
+
+  newproperty(:client_certificate_ca_file) do
+    desc "The CA to use to validate client certificates"
+  end
+
+  newproperty(:peer_certification_mode) do
+    desc "Sets the peer certification modes for the specified client SSL profiles."
+    newvalues(/^CLIENTSSL_CERTIFICATE_MODE_(REQUEST|REQUIRE|IGNORE|AUTO):(true|false)$/)
+  end
+end
