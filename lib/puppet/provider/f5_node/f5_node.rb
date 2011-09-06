c915162607f1be7c80f0e73c0948e98c3cc6d600
Brenton Leanhardt <bleanhar@redhat.com>
2011-09-06 14:49:39 -0400

    Minor fixes to the node type
    Without these the node type will not work

--- lib/puppet/provider/f5_node/f5_node.rb
+++ lib/puppet/provider/f5_node/f5_node.rb
@@ -29,7 +29,7 @@ Puppet::Type.type(:f5_node).provide(:f5_node, :parent => Puppet::Provider::F5) d
   methods.each do |method|
     define_method(method.to_sym) do
       if transport[wsdl].respond_to?("get_#{method}".to_sym)
-        transport[wsdl].send("get_#{method}", resource[:name]).first
+        transport[wsdl].send("get_#{method}", resource[:name]).first.to_s
       end
     end
   end
@@ -44,11 +44,12 @@ Puppet::Type.type(:f5_node).provide(:f5_node, :parent => Puppet::Provider::F5) d
 
   def connection_limit
     val = transport[wsdl].get_connection_limit(resource[:name]).first
-    [val.high, val.low]
+    [val.high.to_s, val.low.to_s]
   end
 
   def connection_limit=(value)
-    transport[wsdl].set_connection_limit(resource[:name], resource[:connection_limit])
+    limit = {:high => resource[:connection_limit][0].to_i, :low => resource[:connection_limit][1].to_i}
+    transport[wsdl].set_connection_limit(resource[:name], [limit])
   end
 
   def monitor_association
