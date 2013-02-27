35f2f01da6c96324e12fa0254236ecffbf7ebba8
James Turnbull <james@lovedthanlost.net>
2013-02-27 03:59:34 +0000

    Fixed linting error for params class Fixed soap4r gem requirement Added back shared support examples

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,15 @@
+# Class: f5::params
+#
+#   Deploy necessary component to manage F5 devices on proxy systems.
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
 class f5::params {
   if $::puppetversion =~ /Puppet Enterprise/ {
     $owner    = 'pe-puppet'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
-
+require 'support/shared_type_examples_spec'
